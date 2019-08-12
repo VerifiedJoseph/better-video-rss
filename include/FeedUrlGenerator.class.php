9798315c6ca1f01967065eb72d39e785a9cbeb34
Joseph <joseph@verifiedjoseph.com>
2019-08-12 14:25:54 +0100

    [FeedUrlGenerator] Add class

---
+++ include/FeedUrlGenerator.class.php
@@ -0,0 +1,170 @@
+<?php
+
+use \Curl\Curl;
+
+class FeedUrlGenerator {
+	/**
+	 * @var string $query Search query
+	 */
+	private $query = '';
+
+	/**
+	 * @var string $channelId YouTube channel ID
+	 */
+	private $channelId = '';
+
+	/**
+	 * @var boolean $error Error status
+	 */
+	private $error = false;
+
+	/**
+	 * @var string $errorMessage Error Message
+	 */
+	private $errorMessage = '';
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->checkInputs();
+
+		if (!empty($this->query)) {
+			$this->findChannel();
+		}
+
+		$this->display();
+	}
+
+	/**
+	 * Check user inputs
+	 *
+	 * @throws Exception if a query is not given
+	 */
+	private function checkInputs() {
+
+		if (isset($_POST['query'])) {
+
+			if (empty($_POST['query'])) {
+				throw new Exception('Query parameter not given.');
+			}
+
+			$this->query = $_POST['query'];
+		}
+	}
+
+	/**
+	 * Display HTML
+	 *
+	 * @return string $html
+	 */
+	private function display() {
+
+		$link = '';
+		$error = '';
+
+		if (!empty($this->channelId) && $this->error === false) {
+			$link = <<<HTML
+<p>Feed URL: <a href="../BetterYouTubeRss.php?channel_id={$this->channelId}">../BetterYouTubeRss.php?channel_id={$this->channelId}</a></p>
+HTML;
+		}
+
+		if ($this->error) {
+			$link = <<<HTML
+<p>{$this->errorMessage}</p>
+HTML;
+		}
+
+		$html = <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Feed URL Generator</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<div id="header">Feed URL Generator</div>
+	<div id="main">
+		<form action="" method="post">
+			<input style="width:280px;" name="query" type="input" placeholder="Username, Channel ID or Channel Title">
+			<button style="width:80px;" type="submit">Generate</button>
+		</form><br>
+		{$link}{$error}
+	</div>
+</body>
+</html>
+HTML;
+
+		echo $html;
+	}
+
+	/**
+	 * Find channel
+	 */
+	private function findChannel() {
+
+		if ($this->isChannelId($this->query) === true) {
+			$this->channelId = $this->query;
+
+		} else {
+			$this->searchApi($this->query);
+		}
+	}
+
+	/**
+	 * Is query string a channel ID
+	 *
+	 8 param string $query Query string
+	 * @return boolean
+	 */
+	private function isChannelId(string $query) {
+
+		if (substr($query, 0, 2) === 'UC' && mb_strlen($query, 'utf8') >= 24) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Is query string a channel ID
+	 *
+	 8 param string $query Query string
+	 * @throws Exception If a curl error has occurred
+	 * @throws Exception If a API error has occurred
+	 * @throws Exception If the channel was not found
+	 */
+	private function searchApi(string $query) {
+
+		try {
+
+			$url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&fields=items(snippet(channelId))&q=' .
+				urlencode($query) . '&type=channel&maxResults=1&prettyPrint=false&key=' . Config::get('YouTubeApiKey');
+
+			$curl = new Curl();
+			$curl->get($url);
+
+			$statusCode = $curl->getHttpStatusCode();
+			$errorCode = $curl->getCurlErrorCode();
+			$response =	$curl->response;
+
+			if ($errorCode !== 0) {
+				throw new Exception('Error: ' . $curl->errorCode . ': ' . $curl->errorMessage);
+			}
+
+			if ($statusCode !== 200) {
+				throw new Exception('An API error occurred.');
+			}
+
+			if (empty($response->items)) {
+				throw new Exception('Channel not found');
+			}
+
+			$this->channelId = $response->items['0']->snippet->channelId;
+
+		} catch (Exception $e) {
+			$this->error = true;
+			$this->errorMessage = $e->getMessage();
+		}
+	}
+}
