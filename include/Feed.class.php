7116320352a3c5307198e01e57b8d9ccea00cbcb
Joseph <joseph@verifiedjoseph.com>
2019-08-26 11:13:42 +0100

    [Feed] Improve URL regex
    Add suppot for URL paths containing exclamation marks and commas

--- include/Feed.class.php
+++ include/Feed.class.php
@@ -5,7 +5,7 @@ class Feed {
 	private $data = array();
 	private $feed = '';
 
-	private $urlRegex = '/https?:\/\/(?:www\.)?(?:[a-zA-Z0-9-.]{2,256}\.[a-z]{2,20})(\:[0-9]{2,4})?(?:\/[a-zA-Z0-9@:%_\+.~#?&\/\/=\-*]+|\/)?/';
+	private $urlRegex = '/https?:\/\/(?:www\.)?(?:[a-zA-Z0-9-.]{2,256}\.[a-z]{2,20})(\:[0-9]{2,4})?(?:\/[a-zA-Z0-9@:%_\+.,~#"!?&\/\/=\-*]+|\/)?/';
 
 	private $embedUrl = 'https://www.youtube.com';
 	private $embedUrlNoCookie = 'https://www.youtube-nocookie.com';
