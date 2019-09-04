89fa068cc8d0e02681a0d2054f755fdeadfd75cb
Joseph <joseph@verifiedjoseph.com>
2019-09-04 16:22:42 +0000

    Revert "Add playlist support (#27)"
    This reverts commit cd0a2c94fb1b28dd397f5ea522e194ad73fb046c.

--- include/FeedUrlGenerator.class.php
+++ include/FeedUrlGenerator.class.php
@@ -3,42 +3,28 @@
 use \Curl\Curl;
 
 class FeedUrlGenerator {
-
-	/** @var string $apiEndpoint YouTube API Endpoint */
-	private $apiEndpoint = 'https://www.googleapis.com/youtube/v3/';
-
 	/**
 	 * @var string $query Search query
 	 */
 	private $query = '';
 
-	/**
-	 * @var boolean $embedVideos Embed videos status
+	/** 
+	 * @var boolean $embedVideos Embed videos status 
 	 */
 	private $embedVideos = false;
-
+	
 	/**
-	 * @var string $feedId YouTube channel or playlist ID
+	 * @var string $channelId YouTube channel ID
 	 */
-	private $feedId = '';
+	private $channelId = '';
 
 	/**
-	 * @var string $feedType Feed type (channel or playlist)
-	 */
-	private $feedType = 'channel';
-
-	/**
-	 * @var array $supportedTypes Supported feed types
-	 */
-	private $supportedTypes = array('channel', 'playlist');
-
-	/**
-	 * @var string $feedFormat Feed Format
+	 * @var string $feedFormat Feed Format 
 	 */
 	private $feedFormat = 'rss';
 
 	/**
-	 * @var array $supportedFormats Supported feed formats
+	 * @var array $supportedFormats Supported feed formats 
 	 */
 	private $supportedFormats = array('rss', 'html');
 
@@ -59,22 +45,14 @@ class FeedUrlGenerator {
 		$this->checkInputs();
 
 		if (!empty($this->query)) {
-
-			if ($this->feedType === 'channel') {
-				$this->findChannel();
-			}
-
-			if ($this->feedType === 'playlist') {
-				$this->findPlaylist();
-			}
+			$this->findChannel();
 		}
 	}
 
 	/**
 	 * Check user inputs
 	 *
-	 * @throws Exception if a query parameter is not given
-	 * @throws Exception if a type parameter is not given
+	 * @throws Exception if a query is not given
 	 */
 	private function checkInputs() {
 
@@ -83,25 +61,17 @@ class FeedUrlGenerator {
 			if (empty($_POST['query'])) {
 				throw new Exception('Query parameter not given.');
 			}
-
-			if (empty($_POST['type'])) {
-				throw new Exception('Type parameter not given.');
-			}
-
-			if (isset($_POST['type']) && in_array($_POST['type'], $this->supportedTypes)) {
-				$this->feedType = $_POST['type'];
-			}
-
+			
 			if (isset($_POST['format']) && in_array($_POST['format'], $this->supportedFormats)) {
 				$this->feedFormat = $_POST['format'];
 			}
-
+			
 			$this->query = $_POST['query'];
-
+			
 			if (isset($_POST['embed_videos'])) {
 				$this->embedVideos = true;
 			}
-
+			
 		}
 	}
 
@@ -114,35 +84,23 @@ class FeedUrlGenerator {
 
 		$link = '';
 		$error = '';
-		$channelLink = '';
-		$channelError = '';
-		$playlistLink = '';
-		$playlistError = '';
 
-		if (!empty($this->feedId) && $this->error === false) {
-			$url = Config::get('SELF_URL_PATH') . '?' . $this->feedType . '=' . $this->feedId . '&format=' . $this->feedFormat;
-
+		if (!empty($this->channelId) && $this->error === false) {
+			$url = Config::get('SELF_URL_PATH') . '?channel_id=' . $this->channelId . '&format=' . $this->feedFormat;
+			
 			if ($this->embedVideos === true) {
 				$url .= '&embed_videos=true';
 			}
-
+			
 			$link = <<<HTML
 <p>Feed URL: <a href="{$url}">{$url}</a></p>
 HTML;
+		}
 
-			$error = <<<HTML
+		if ($this->error) {
+			$link = <<<HTML
 <p>{$this->errorMessage}</p>
 HTML;
-		}
-
-		if ($this->feedType === 'channel') {
-			$channelLink = $link;
-			$channelError = $error;
-		}
-
-		if ($this->feedType === 'playlist') {
-			$playlistLink = $link;
-			$playlistError = $error;
 		}
 
 			$html = <<<HTML
@@ -160,7 +118,6 @@ HTML;
 	<div id="main">
 		<div id="items">
 			<div class="item">
-				<h2>Channel</h2>
 				<form action="" method="post">
 					Channel: <input style="width:280px;" name="query" type="input" placeholder="Username, Channel ID or Channel Title"><br>
 					Embed videos: <input type="checkbox" name="embed_videos" value="yes"><br>
@@ -169,25 +126,9 @@ HTML;
 						<option value="rss">RSS</option>
 						<option value="html">html</option>
 					</select><br/>
-					<input type="hidden" name="type" value="channel">
 					<button style="width:80px;" type="submit">Generate</button>
 				</form><br>
-				{$channelLink}{$channelError}
-			</div>
-			<div class="item">
-				<h2>Playlist</h2>
-				<form action="" method="post">
-					Playlist: <input style="width:280px;" name="query" type="input" placeholder="Playlist ID or title"><br>
-					Embed videos: <input type="checkbox" name="embed_videos" value="yes"><br>
-					Feed format: 
-					<select name="format">
-						<option value="rss">RSS</option>
-						<option value="html">html</option>
-					</select><br/>
-					<input type="hidden" name="type" value="playlist">
-					<button style="width:80px;" type="submit">Generate</button>
-				</form><br>
-				{$playlistLink}{$playlistError}
+				{$link}{$error}
 			</div>
 			<div class="item">
 				<a href="tools">Tools</a> - <a href="https://github.com/VerifiedJoseph/BetterYouTubeRss">Source Code</a>
@@ -207,20 +148,7 @@ HTML;
 	private function findChannel() {
 
 		if ($this->isChannelId($this->query) === true) {
-			$this->feedId = $this->query;
-
-		} else {
-			$this->searchApi($this->query);
-		}
-	}
-
-	/**
-	 * Find playlist
-	 */
-	private function findPlaylist() {
-
-		if ($this->isPlaylistId($this->query) === true) {
-			$this->feedId = $this->query;
+			$this->channelId = $this->query;
 
 		} else {
 			$this->searchApi($this->query);
@@ -243,21 +171,6 @@ HTML;
 	}
 
 	/**
-	 * Is query string a playlist ID
-	 *
-	 * @param string $query Query string
-	 * @return boolean
-	 */
-	private function isPlaylistId(string $query) {
-
-		if (substr($query, 0, 2) === 'PL' && mb_strlen($query, 'utf8') >= 34) {
-			return true;
-		}
-
-		return false;
-	}
-
-	/**
 	 * Is query string a channel ID
 	 *
 	 * @param string $query Query string
@@ -269,17 +182,8 @@ HTML;
 
 		try {
 
-			if ($this->feedType === 'channel') {
-				$url = $this->apiEndpoint . 'search?part=snippet&fields=items(snippet(channelId))&q='
-					. urlencode($query) . '&type=channel';
-			}
-
-			if ($this->feedType === 'playlist') {
-				$url = $this->apiEndpoint . 'search?part=snippet&fields=items(id(playlistId))&q='
-					. urlencode($query) . '&type=playlist';
-			}
-
-			$url .= '&maxResults=1&prettyPrint=false&key=' . Config::get('YOUTUBE_API_KEY');
+			$url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&fields=items(snippet(channelId))&q=' .
+				urlencode($query) . '&type=channel&maxResults=1&prettyPrint=false&key=' . Config::get('YOUTUBE_API_KEY');
 
 			$curl = new Curl();
 			$curl->get($url);
@@ -297,16 +201,10 @@ HTML;
 			}
 
 			if (empty($response->items)) {
-				throw new Exception($this->feedType . ' not found');
+				throw new Exception('Channel not found');
 			}
 
-			if ($this->feedType === 'channel') {
-				$this->feedId = $response->items['0']->snippet->channelId;
-			}
-
-			if ($this->feedType === 'playlist') {
-				$this->feedId = $response->items['0']->id->playlistId;
-			}
+			$this->channelId = $response->items['0']->snippet->channelId;
 
 		} catch (Exception $e) {
 			$this->error = true;
