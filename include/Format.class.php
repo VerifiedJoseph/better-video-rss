f6673c6a854089d62a0d6e624c2022f0bb5cda8c
Joseph <joseph@verifiedjoseph.com>
2019-09-05 10:49:36 +0000

    [Format] Remove 'abstract' keyword from buildContent() (#34)
    Remove abstract keyword from buildContent() in Format and remove the function from extended classes HtmlFormat, XmlFormat and JsonFormat. Code in buildContent() is not modified by the extended classes so there is no need to force extending classes to define the function.
    
    * [Format] Remove 'abstract' keyword from buildContent()
    * [XmlFormat] Remove function buildContent()
    * [HtmlFormat] Remove function buildContent()
    * [JsonFormat] Remove function buildContent()

--- include/Format.class.php
+++ include/Format.class.php
@@ -61,7 +61,32 @@ abstract class Format {
 	 *
 	 * @param array $video Video data
 	 */
-	abstract protected function buildContent(array $video);
+	protected function buildContent(array $video) {
+		
+		$description = $this->formatDescription($video['description']);
+		$published = Helper::convertUnixTime($video['published'], config::get('DATE_FORMAT'));
+
+		$media = <<<EOD
+<a target="_blank" title="Watch" href="https://youtube.com/watch?v={$video['id']}"><img src="{$video['thumbnail']}"/></a>
+EOD;
+
+		if ($this->embedVideos === true) {
+			$url = $this->embedUrl;
+
+			if (config::get('YOUTUBE_EMBED_PRIVACY')) {
+				$url = $this->embedUrlNoCookie;
+			}
+
+		$media = <<<EOD
+<iframe width="100%" height="410" src="{$url}/embed/{$video['id']}" frameborder="0" allow="encrypted-media;" allowfullscreen></iframe>
+EOD;
+		}
+
+		return <<<EOD
+<a target="_blank" title="Watch" href="https://youtube.com/watch?v={$video['id']}">{$media}</a>
+<hr/>Published: {$published} - Duration: {$video['duration']}<hr/><p>{$description}</p>
+EOD;
+	}
 
 	/**
 	 * Format video description
