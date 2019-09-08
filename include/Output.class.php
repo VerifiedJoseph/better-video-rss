57611d9e43f0b147d229499f07a7de8f21b511bb
Joseph <joseph@verifiedjoseph.com>
2019-09-08 16:37:42 +0100

    [Output] Remove functions xml(), json() & html()
    Remove functions xml(), json() & html() which are unnecessary as of https://github.com/VerifiedJoseph/BetterYouTubeRss/commit/2694dca502c1235f326b62b3cf6cb6a6c51b5210

--- include/Output.class.php
+++ include/Output.class.php
@@ -11,10 +11,6 @@ class Output {
 
 	/**
 	 * Output feed
-	 */	
-	
-	/**
-	 * Output feed
 	 *
 	 * @param string $data Feed data
 	 * @param string $contentType Content-type header value
@@ -22,29 +18,5 @@ class Output {
 	public static function feed(string $data, string $contentType) {
 		header('content-type: ' . $contentType);
 		echo $data;
-	}
-	
-	/**
-	 * Output XML
-	 */
-	public static function xml(string $feed) {
-		header('content-type: text/xml; charset=UTF-8');
-		echo $feed;
-	}
-
-	/**
-	 * Output JSON
-	 */
-	public static function json(string $feed) {
-		header('content-type: application/json');
-		echo $feed;
-	}
-
-	/**
-	 * Output HTML
-	 */
-	public static function html(string $feed) {
-		header('content-type: text/html; charset=UTF-8');
-		echo $feed;
 	}
 }
