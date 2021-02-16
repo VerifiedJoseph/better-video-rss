ec1a0dc6beadaf98cec0ad8ec9b910465931043d
Joseph <joseph@verifiedjoseph.com>
2021-02-16 15:35:35 +0000

    [Helper\Validate] Add class

---
+++ include/Helper/Validate.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Helper;
+
+use Configuration as Config;
+
+class Validate {
+
+	/** @var string $channelIdRegex channel ID regex */
+	private static string $channelIdRegex = '/^UC[a-zA-Z0-9_-]+$/';
+
+	/** @var string $playlistIdRegex playlist ID regex */
+	private static string $playlistIdRegex = '/^(?:UU|PL)[a-zA-Z0-9_-]+$/';
+
+	/**
+	 * Validate a feed format
+	 *
+	 * @param string $format Feed format
+	 * @return boolean
+	 */
+	public static function feedFormat(string $format) {
+		if (in_array($format, Config::getFeedFormats())) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Validate a channel ID
+	 *
+	 * @param string $channelId
+	 * @return boolean
+	 */
+	public static function channelId(string $channelId) {
+		if(preg_match(self::$channelIdRegex, $channelId)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Validate a playlist ID
+	 *
+	 * @param string $playlistId
+	 * @return boolean
+	 */
+	public static function playlistId(string $playlistId) {
+		if(preg_match(self::$playlistIdRegex, $playlistId)) {
+			return true;
+		}
+
+		return false;
+	}
+}
