ffc3ac30b46e9aace86d99fd5651bc77cea98b50
Joseph <joseph@verifiedjoseph.com>
2021-03-28 21:47:18 +0100

    [feed.php] Add file

---
+++ feed.php
@@ -0,0 +1,22 @@
+<?php
+
+// Composer Auto loader
+require __DIR__ . '/vendor/autoload.php';
+
+// Class Auto loader
+require __DIR__ . '/autoload.php';
+
+use Configuration as Config;
+use Helper\Output;
+
+try {
+	Config::checkInstall();
+	Config::checkConfig();
+
+	$feed = new Feed();
+	$feed->generate();
+	$feed->output();
+
+} catch (Exception $e) {
+	Output::Error($e->getMessage());
+}
