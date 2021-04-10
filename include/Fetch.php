5baa7147f3b1465e1c6c6eb83f49e6c592e4a1d0
Joseph <joseph@verifiedjoseph.com>
2021-04-10 17:17:06 +0100

    [Fetch] Add method thumbnail()

--- include/Fetch.php
+++ include/Fetch.php
@@ -31,4 +31,30 @@ class Fetch {
 
 		return $curl->getResponse();
 	}
+
+	/**
+	 * Fetch YouTube thumbnail
+	 *
+	 * @param string $url YouTube thumbnail URL
+	 *
+	 * @return object|array Response from Curl
+	 *
+	 * @throws Exception If a curl error has occurred.
+	 * @throws Exception If failed to fetch image.
+	 */
+	public function thumbnail(string $url) {
+		$curl = new Curl();
+		$curl->get($url);
+		$this->response = $curl->response;
+
+		if ($curl->getCurlErrorCode() !== 0) {
+			throw new Exception('Error: ' . $curl->getCurlErrorCode() . ': ' . $curl->errorMessage);
+		}
+
+		if ($curl->getHttpStatusCode() !== 200) {
+			throw new Exception('Failed to fetch: ' . $url);
+		}
+
+		return $curl->getResponse();
+	}
 }
