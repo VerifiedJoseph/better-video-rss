2eeb2d78a3aaa58852f1473d276c1cc7bbeed1b9
Joseph <joseph@verifiedjoseph.com>
2021-10-21 17:33:49 +0100

    [Helper\Format] Add class & method minify()

---
+++ include/Helper/Format.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Helper;
+
+class Format {
+
+	/**
+	 * Minify HTML and XML
+	 * 
+	 * @param string $data 
+	 * @return string $data Returns minified data
+	 */
+	public static function minify($data) {
+		$search = array(
+			'/\>[^\S ]+/s', // strip white spaces after tags, except space
+			'/[^\S ]+\</s', // strip white spaces before tags, except space
+			'/(\s)+/s' // shorten multiple white space sequences
+		);
+
+		$replace = array('>','<','\\1');
+		$data = preg_replace($search, $replace, $data);
+	
+		return $data;
+	}
+}
