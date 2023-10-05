791560a4de615526e42090d8ce0eb22d484db693
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-10-05 16:42:15 +0100

    Update Rss.php

--- include/Format/Rss.php
+++ include/Format/Rss.php
@@ -14,7 +14,7 @@ class Rss extends Format
     /**
      * Build feed
      */
-    public function build()
+    public function build(): void
     {
         $feedDescription = $this->xmlEncode($this->data['details']['description']);
         $feedTitle = $this->xmlEncode($this->data['details']['title']);
@@ -105,10 +105,10 @@ XML;
     /**
      * Build item categories
      *
-     * @param array $categories Item categories
+     * @param array<int, string> $categories Item categories
      * @return string Categories as XML
      */
-    protected function buildCategories(array $categories)
+    protected function buildCategories(array $categories): string
     {
         $itemCategories = '';
 
