c5d804eae37dbf2f1f6706d5c09cbfb7ce52d83d
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-10-06 13:23:03 +0100

    Create Template.php

---
+++ include/Template.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use App\Helper\File;
+use App\Exception\ConfigurationException;
+
+/**
+ * Class for rendering pages using HTML templates
+ */
+class Template
+{
+    private string $html = '';
+
+    /** @var array<string, string> $variables Template variables */
+    private array $variables = [];
+
+    /**
+     * @param string $name Template name
+     * @param array<string, string> $variables Template variables
+     */
+    function __construct(string $name, array $variables = [])
+    {
+        $this->variables = $variables;
+        $this->load($name);
+    }
+
+    /**
+     * Render template into page
+     */
+    public function render(): string
+    {
+        foreach ($this->variables as $name => $value) {
+            $name = sprintf('{%s}', $name);
+            $this->html = str_replace($name, $value, $this->html);
+        }
+
+        return $this->html;
+    }
+
+    /**
+     * Load Template
+     * 
+     * @var string $name Template name
+     * @throws ConfigException if template file is not found.
+     */
+    private function load(string $name): void
+    {
+        $path = 'include/templates/' . $name . '.html';
+
+        if (file_exists($path) === false) {
+            throw new ConfigurationException('Template file not found: ' . $path);
+        }
+
+        $this->html = File::read($path);
+    }
+}
