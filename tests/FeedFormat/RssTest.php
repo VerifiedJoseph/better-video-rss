2414ba9a18e682e65be74022ee887433d02f837c
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-10-18 12:06:17 +0100

    Add RssTest

---
+++ tests/FeedFormat/RssTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Config;
+use App\FeedFormat\Rss;
+
+class RssTest extends TestCase
+{
+    private Config $config;
+
+    /** @var array<string, mixed> $data */
+    private array $data = [];
+
+    /**
+     * @return Config&PHPUnit\Framework\MockObject\Stub
+     */
+    private function createConfigStub(): Config
+    {
+        $config = $this->createStub(Config::class);
+        $config->method('getImageProxyStatus')->willReturn(false);
+        $config->method('getSelfUrl')->willReturn('https://example.com/');
+        $config->method('getTimezone')->willReturn('Europe/London');
+        $config->method('getDateFormat')->willReturn('F j, Y');
+        $config->method('getTimeFormat')->willReturn('H:i');
+
+        return $config;
+    }
+
+    public function setUp(): void
+    {
+        $this->data = (array) json_decode((string) file_get_contents('tests/files/channel-cache-data.json'), true);
+        $this->config = $this->createConfigStub();
+    }
+
+    /**
+     * Test `build()`
+     */
+    public function testBuild(): void
+    {
+        $format = new Rss($this->data, false, $this->config);
+        $format->build();
+
+		$expected = 'tests/files/FeedFormats/expected-rss-feed.xml';
+        $this->assertXmlStringEqualsXmlFile(
+            $expected,
+            $format->get()
+        );
+    }
+
+    /**
+     * Test `getContentType()`
+     */
+    public function testGetContentType(): void
+    {
+        $format = new Rss($this->data, false, $this->config);
+        $format->build();
+
+        $this->assertEquals('text/xml; charset=UTF-8', $format->getContentType());
+    }
+
+    /**
+     * Test `getLastModified()`
+     */
+    public function testGetLastModified(): void
+    {
+        $format = new Rss($this->data, false, $this->config);
+        $format->build();
+
+        $this->assertEquals('Wed, 18 Oct 2023 11:22:06 BST', $format->getLastModified());
+    }
+}
