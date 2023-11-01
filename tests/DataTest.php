49ab3fc3833081dab6840a4ec8ecf662dc1b4d03
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-11-01 20:14:57 +0000

    Create DataTest.php

---
+++ tests/DataTest.php
@@ -0,0 +1,173 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Data;
+use App\Config;
+use App\Helper\Convert;
+
+class DataTest extends TestCase
+{
+    private static Data $data;
+
+    private static array $channelCacheData;
+    private static stdClass $apiResponses;
+
+    private static string $cacheFilepath = '';
+
+    private static string $feedId = 'UCBa659QWEk1AI4Tg--mrJ2A';
+    private static string $feedType = 'channel';
+
+    /**
+     * @return PHPUnit\Framework\MockObject\Stub&Config
+     */
+    private static function createConfigStub(): Config
+    {
+        /** @var PHPUnit\Framework\MockObject\Stub&Config */
+        $config = self::createStub(Config::class);
+        $config->method('getCacheDisableStatus')->willReturn(false);
+        $config->method('getCacheDirPath')->willReturn(sys_get_temp_dir());
+        $config->method('getCacheFileExtension')->willReturn('cache');
+
+        return $config;
+    }
+
+    private static function createCacheFile(): void
+    {
+        self::$channelCacheData = (array) json_decode(
+            (string)
+            file_get_contents('tests/files/channel-cache-data.json'),
+            associative: true
+        );
+
+        self::$apiResponses = json_decode(
+            (string) file_get_contents('tests/files/data-class-response-samples.json'),
+        );
+
+        self::$cacheFilepath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . hash('sha256', self::$feedId) . '.cache';
+
+        copy('tests/files/channel-cache-data.json', self::$cacheFilepath);
+    }
+
+    public static function setUpBeforeClass(): void
+    {
+        self::createCacheFile();
+        $config = self::createConfigStub();
+
+        self::$data = new Data(
+            self::$feedId,
+            self::$feedType,
+            $config
+        );
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        unlink(self::$cacheFilepath);
+    }
+
+    /**
+     * Test `getData()`
+     */
+    public function testGetData(): void
+    {
+        $this->assertEquals(self::$channelCacheData, self::$data->getData());
+    }
+
+    /**
+     * Test `getPartEtag()`
+     */
+    public function testGetPartEtag(): void
+    {
+        $expected = 'Q4tjoKjLcrpOC7-WzcBdtCxY0kg';
+
+        $this->assertEquals($expected, self::$data->getPartEtag('details'));
+    }
+
+    /**
+     * Test `getExpiredParts()`
+     */
+    public function testGetExpiredParts(): void
+    {
+        $expected = ['details', 'feed', 'videos'];
+
+        $this->assertEquals($expected, self::$data->getExpiredParts());
+    }
+
+    /**
+     * Test `getExpiredVideos()`
+     */
+    public function testGetExpiredVideos(): void
+    {
+        $expected = 'CkZyZFa5qO0,ZgDBIzClmPg';
+
+        $this->assertEquals($expected, self::$data->getExpiredVideos());
+    }
+
+    /**
+     * Test `updateDetails()`
+     */
+    public function testUpdateDetails(): void
+    {
+        self::$data->updateDetails(self::$apiResponses->details);
+        $data = self::$data->getData();
+
+        $etag = self::$apiResponses->details->etag;
+        $title = self::$apiResponses->details->items[0]->snippet->title;
+        $description = self::$apiResponses->details->items[0]->snippet->description;
+        $thumbnail = self::$apiResponses->details->items[0]->snippet->thumbnails->default->url;
+
+        $this->assertEquals($etag, $data['details']['etag']);
+        $this->assertEquals($title, $data['details']['title']);
+        $this->assertEquals($description, $data['details']['description']);
+        $this->assertEquals($thumbnail, $data['details']['thumbnail']);
+        $this->assertGreaterThan(self::$channelCacheData['details']['expires'], $data['details']['expires']);
+        $this->assertGreaterThan(self::$channelCacheData['details']['fetched'], $data['details']['fetched']);
+    }
+
+    /**
+     * Test `updateVideos()`
+     */
+    public function testUpdateVideos(): void
+    {
+        self::$data->updateVideos(self::$apiResponses->videos);
+        $data = self::$data->getData();
+
+        $duration = '03:45';
+        $tags = self::$apiResponses->videos->items[0]->snippet->tags;
+        $thumbnail = 'https://i.ytimg.com/vi/CkZyZFa5qO0/sddefault.jpg';
+
+        $this->assertEquals($duration, $data['videos'][0]['duration']);
+        $this->assertEquals($tags, $data['videos'][0]['tags']);
+        $this->assertEquals($thumbnail, $data['videos'][0]['thumbnail']);
+        $this->assertGreaterThan(self::$channelCacheData['videos'][0]['expires'], $data['videos'][0]['expires']);
+        $this->assertGreaterThan(self::$channelCacheData['videos'][0]['fetched'], $data['videos'][0]['fetched']);
+    }
+
+    /**
+     * Test `updateFeed()`
+     */
+    public function testUpdateFeed(): void
+    {
+        $rssFeedResponse = (string) file_get_contents('tests/files/data-class-rss-feed-response-sample.xml');
+
+        self::$data->updateFeed($rssFeedResponse);
+        $data = self::$data->getData();
+        
+        $xml = simplexml_load_string($rssFeedResponse);
+        $namespaces = $xml->getNamespaces(true);
+
+        $mediaNodes = $xml->entry[0]->children($namespaces['media']);
+
+        $title = (string) $xml->entry[0]->title;
+        $description = (string) $mediaNodes->group->description;
+        $published = strtotime((string) $xml->entry[0]->published);
+        $author = (string) $xml->entry[0]->author->name;
+
+        $this->assertEquals($title, $data['videos'][0]['title']);
+        $this->assertEquals($description, $data['videos'][0]['description']);
+        $this->assertEquals($published, $data['videos'][0]['published']);
+        $this->assertEquals($author, $data['videos'][0]['author']);
+        $this->assertGreaterThan(self::$channelCacheData['feed']['expires'], $data['feed']['expires']);
+        $this->assertGreaterThan(self::$channelCacheData['feed']['fetched'], $data['feed']['fetched']);
+    }
+}
