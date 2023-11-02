47f04f13631e6cadf7b8d83de5712c0307114a2a
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-11-02 12:46:50 +0000

    Create IndexTest.php

---
+++ tests/IndexTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Config;
+use App\Api;
+use App\Index;
+
+class IndexTest extends TestCase
+{
+    private static Config $config;
+    private static Api $api;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$config = new Config();
+        self::$api = new Api(self::$config);
+    }
+
+    /**
+     * Test class with empty query
+     */
+    public function testClassWithEmptyQuery(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Query parameter not given.');
+
+        $inputs = [
+            'query' => ''
+        ];
+
+        new Index($inputs, self::$config, self::$api);
+    }
+
+    /**
+     * Test class with empty type
+     */
+    public function testClassWithEmptyType(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Type parameter not given.');
+
+        $inputs = [
+            'query' => 'Hello World',
+            'type' => ''
+        ];
+
+        new Index($inputs, self::$config, self::$api);
+    }
+
+    /**
+     * Test class with unsupported type
+     */
+    public function testClassWithUnsupportedType(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Unknown type parameter given.');
+
+        $inputs = [
+            'query' => 'Hello World',
+            'type' => 'fake-type-here'
+        ];
+
+        new Index($inputs, self::$config, self::$api);
+    }
+}
