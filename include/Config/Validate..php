4befa1ea92cd4ccfcd89fbb89835c2c47d84bed3
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-04-09 14:48:04 +0100

    Update Validate..php

--- include/Config/Validate..php
+++ include/Config/Validate..php
@@ -2,6 +2,8 @@
 
 namespace App\Config;
 
+use App\Exception\ConfigException;
+
 class Validate extends Base
 {
     /** @var array<string, mixed> $config Config */
@@ -23,5 +25,34 @@ class Validate extends Base
     public function getConfig(): array
     {
         return $this->config;
+    }
+
+    /**
+     * Check php version
+     *
+     * @param string $version php version
+     * @param string $minimumVersion Minimum required PHP version
+     * @throws ConfigException if PHP version not supported.
+     */
+    public function version(string $version, string $minimumVersion): void
+    {
+        if (version_compare($version, $minimumVersion) === -1) {
+            throw new ConfigException('BetterVideoRss requires at least PHP version ' . $minimumVersion);
+        }
+    }
+
+    /**
+     * Check for required php extensions
+     *
+     * @param array<int, string> $required Required php extensions
+     * @throws ConfigException if a required PHP extension is not loaded.
+     */
+    public function extensions(array $required): void
+    {
+        foreach ($required as $ext) {
+            if (extension_loaded($ext) === false) {
+                throw new ConfigException(sprintf('PHP extension error: %s extension not loaded.', $ext));
+            }
+        }
     }
 }
