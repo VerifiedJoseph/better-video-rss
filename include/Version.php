d214c7c725ebc32328b8c8d6cc2ee100b139418c
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-06-19 15:21:22 +0000

    Version 1.8.2 (#341)

--- include/Version.php
+++ include/Version.php
@@ -4,7 +4,7 @@ namespace App;
 
 class Version
 {
-    private static string $version = '1.8.1';
+    private static string $version = '1.8.2';
     private static int $cacheFormatVersion = 1;
 
     /**
