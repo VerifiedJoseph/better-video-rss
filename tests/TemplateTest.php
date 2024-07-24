2647998de13f2d1abaf9da5da2286c347b4cd831
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-07-24 15:46:38 +0100

    Update TemplateTest.php

--- tests/TemplateTest.php
+++ tests/TemplateTest.php
@@ -1,9 +1,16 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
 use App\Template;
 use App\Exception\ConfigException;
+use App\Helper\Format;
 
+#[CoversClass(Template::class)]
+#[UsesClass(ConfigException::class)]
+#[UsesClass(App\Helper\File::class)]
+#[UsesClass(App\Helper\Format::class)]
 class TemplateTest extends TestCase
 {
     /**
@@ -24,6 +31,29 @@ class TemplateTest extends TestCase
         $template = new Template('feed.xml', $variables);
 
         $this->assertXmlStringEqualsXmlFile('tests/files/expected-rss-feed.xml', $template->render());
+    }
+
+    /**
+     * Test render() method with minification enabled
+     */
+    public function testRenderWithMinification(): void
+    {
+        $expected = Format::minify(
+            file_get_contents('tests/files/expected-rss-feed.xml')
+        );
+
+        $variables = [
+            'feedTitle' => 'phpunit',
+            'feedUrl' => 'https://example.com/feed.rss',
+            'selfUrl' => 'https://example.com',
+            'feedDescription' => 'Feed for phpunit',
+            'feedUpdated' => '1970-01-01 00:00:00',
+            'feedImage' => 'https://example.com/inage.png',
+            'items' => '<items>items here</items>'
+        ];
+
+        $template = new Template('feed.xml', $variables);
+        $this->assertEquals($expected, $template->render(true));
     }
 
     /**
