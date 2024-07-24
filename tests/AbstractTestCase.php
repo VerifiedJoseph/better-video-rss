933a8d7525c997b5a546afc3fb05bad390f4e30c
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-07-24 18:26:09 +0100

    Create AbstractTestCase.php

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+use PHPUnit\Framework\TestCase as TestCase;
+
+use App\Config;
+
+abstract class AbstractTestCase extends TestCase
+{
+    /**
+     * Create config class stub
+     *
+     * Array example: `['getSelfUrl' => 'https://example.com/', 'getTimezone' => 'UTC']`
+     *
+     * @param array<string, mixed> $methods
+     * @return PHPUnit\Framework\MockObject\Stub&Config
+     */
+    protected function createConfigStub(array $methods): Config
+    {
+        /** @var PHPUnit\Framework\MockObject\Stub&Config */
+        $config = $this->createStub(Config::class);
+
+        foreach ($methods as $method => $value) {
+            $config->method($method)->willReturn($value);
+        }
+
+        return $config;
+    }
+}
