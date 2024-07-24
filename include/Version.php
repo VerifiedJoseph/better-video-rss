2252e729b746afd854515eb98ee4b1414a11270b
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-07-24 14:31:57 +0100

    Bump version

--- include/Version.php
+++ include/Version.php
@@ -4,7 +4,7 @@ namespace App;
 
 class Version
 {
-    private static string $version = '1.8.2';
+    private static string $version = '1.8.3';
     private static int $cacheFormatVersion = 1;
 
     /**
